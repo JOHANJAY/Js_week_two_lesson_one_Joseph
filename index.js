e6dab5b16839d36719e3a503727c33748f4c3bb1
John Joseph <joseph.john33@yahoo.com>
2024-09-11 10:33:19 +0100

    submmiting lesson one

---
+++ index.js
@@ -0,0 +1,36 @@
+//Task One
+/* let animals = []
+animals.push('cats', 'dogs', 'goats', 'cows')
+
+console.log(animals); */
+
+//Task Two
+/* let myArr = ['plane', 'cars', 'boats', 'ships', 'trains']
+
+myArr.pop('trains')
+myArr.shift('cars')
+console.log(myArr);
+ */
+
+//Task Three
+/* let myArr = ["plane", "cars", "boats", "ships", "trains"];
+
+myArr[2] = 'cannoe'
+console.log(myArr);
+ */
+
+//Task Four
+/* let myArr = ["plane", "cars", "boats", "ships", "trains"];
+
+myArr.forEach(element => {
+    console.log(`I see ${element}`);
+    
+}); */
+
+//Task Five
+/* let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const oddNumbers = numbers.filter(items => items % 2 == 1)
+
+console.log(oddNumbers);
+ */
